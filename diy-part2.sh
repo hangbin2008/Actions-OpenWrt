dcfacbcc8b22acaf8f4d0dee03edbc6d077da818
hangyubin <hangyubin@163.com>
2021-08-30 15:57:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
 #3. 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/hanbin-wrt/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/hanbin-wrt/g' package/base-files/files/bin/config_generate
 
 #4. 修改banne文件（banne文件在根目录）（不要修改此行代码,怕弄的diy-lede.sh文件全失效,不需要的话前面加#，或者全行代码删除了）
 #rm -rf package/base-files/files/etc/banne && \cp -f banner package/base-files/files/etc/ && cd openwrt
