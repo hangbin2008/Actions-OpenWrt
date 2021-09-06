3814f8fba63e0e78eb2cb99509da87bbd68ad45c
hangyubin <hangyubin@163.com>
2021-09-06 15:11:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,16 +11,16 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.200.210/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.200.210/g' package/base-files/files/bin/config_generate
 
 #2. 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
- sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
 #3. 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
- sed -i 's/OpenWrt/hanbin-wrt/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/hanbin-wrt/g' package/base-files/files/bin/config_generate
 
 #4. 修改banne文件（banne文件在根目录）（不要修改此行代码,怕弄的diy-lede.sh文件全失效,不需要的话前面加#，或者全行代码删除了）
-#rm -rf package/base-files/files/etc/banne && \cp -f banner package/base-files/files/etc/ && cd openwrt
+# rm -rf package/base-files/files/etc/banne && \cp -f banner package/base-files/files/etc/ && cd openwrt
 
 #5. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+# rm openwrt/package/lean/luci-theme-argon -rf
