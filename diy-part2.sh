13d2c00756e570c1c27efbb9f840707bdd048aa3
hangyubin <hangyubin@163.com>
2022-03-29 14:31:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,3 +24,35 @@
 
 #5. Replace with JerryKuKu’s Argon
 # rm openwrt/package/lean/luci-theme-argon -rf
+
+#echo '修改机器名称'
+#sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
+# cpufreq
+# sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+# sed -i 's/services/system/g' package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+# firewall custom
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+
+# Mod zzz-default-settings
+sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" package/lean/default-settings/files/zzz-default-settings
+
+# Openwrt version
+version=$(grep "DISTRIB_REVISION=" package/lean/default-settings/files/zzz-default-settings  | awk -F "'" '{print $2}')
+sed -i '/DISTRIB_REVISION/d' package/lean/default-settings/files/zzz-default-settings
+echo "echo \"DISTRIB_REVISION='${version} $(TZ=UTC-8 date "+%Y.%m.%d") Compilde by hangbin'\" >> /etc/openwrt_release" >> package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit 0/d' package/lean/default-settings/files/zzz-default-settings
+echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
+
+# Add additional packages
+# git clone https://github.com/hangyubin/jell.git package/jell-package
+# git clone https://github.com/kenzok8/small-package package/small-package
+# git clone https://github.com/kenzok8/small.git package/small
+# git clone https://github.com/Boos4721/OpenWrt-Packages.git package/OpenWrt-Packages
+# git clone https://github.com/kenzok8/openwrt-packages.git package/helloworld
+# git clone https://github.com/kenzok8/small-package package/small-package 
+# git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
