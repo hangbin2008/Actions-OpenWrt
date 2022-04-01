2ca5bf3c64c8e049c6cde85b5fbc4b7266166e06
hangyubin <hangyubin@163.com>
2022-04-01 12:35:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # firewall custom
-# echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+ echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
