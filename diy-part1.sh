8027503ffc7f6a193b1df8332b0ed1d29e458aab
hangyubin <hangyubin@163.com>
2022-04-01 23:13:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,19 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # Modify default IP
- sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # firewall custom
  echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+#修改默认ip
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+#删除原主题
+rm -rf package/lean/luci-theme-argon
+#添加新的主题
+# git clone https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
+#添加常用软件包
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+#删除默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+#取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=/luci-static/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
