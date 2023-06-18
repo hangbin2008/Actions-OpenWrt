ece66a6fdca9b710389d44b45a1de3d7e29572be
hangyubin <hangyubin@163.com>
2023-06-18 12:51:07 +0800

    Update Phicomm_N1.sh

--- makeconfig.d/Phicomm_N1.sh
+++ makeconfig.d/Phicomm_N1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 ## 常用OpenWrt软件包源码合集，同步上游更新！
 ## 通用版luci适合18.06与19.07
 # echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
