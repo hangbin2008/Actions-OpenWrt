ee877a637c236cfed174fb6022fc855cc70ae143
hangyubin <hangyubin@163.com>
2023-06-21 10:19:52 +0800

    Update Phicomm_N2.sh

--- makeconfig.d/Phicomm_N2.sh
+++ makeconfig.d/Phicomm_N2.sh
@@ -50,5 +50,5 @@ rm -rf feeds/small8/luci-app-mosdns
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/small8/luci-theme-argon
 rm -rf feeds/small8/v2ray-geodata
-rm -rf feeds/small8/haproxy
-rm -rf feeds/packages/net/haproxy
+#rm -rf feeds/small8/haproxy
+#rm -rf feeds/packages/net/haproxy
