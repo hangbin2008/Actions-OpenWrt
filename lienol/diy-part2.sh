99ae3b0796bdd132ea64b267d077461b203fe651
hangyubin <hangyubin@163.com>
2024-02-13 03:11:11 +0800

    Update diy-part2.sh

--- lienol/diy-part2.sh
+++ lienol/diy-part2.sh
@@ -41,7 +41,7 @@ sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba
 # 拉取软件包
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # git clone https://github.com/hangyubin/homeproxy.git package/homeproxy
-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git clone --depth 1 https://github.com/vernesong/OpenClash.git && mv OpenClash/luci-app-openclash ./package && rm -rf OpenClash
 # 删除重复包
 #rm -rf feeds/luci/applications/luci-app-passwall
 #rm -rf feeds/packages/net/haproxy
