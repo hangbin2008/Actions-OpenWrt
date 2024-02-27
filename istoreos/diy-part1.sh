3db9fbbd42ab3bdea7eec414bade17498c02ccdf
hangyubin <hangyubin@163.com>
2024-02-27 22:07:05 +0800

    Update diy-part1.sh

--- istoreos/diy-part1.sh
+++ istoreos/diy-part1.sh
@@ -17,11 +17,11 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # git clone https://github.com/hangyubin/homeproxy.git package/homeproxy
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-echo 'src-git diskman https://github.com/jjm2473/luci-app-diskman;dev' >> feeds.conf.default
-echo 'src-git third_party https://github.com/linkease/istore-packages.git;main' >> feeds.conf.default
-echo 'src-git jjm2473_apps https://github.com/jjm2473/openwrt-apps.git;main' >> feeds.conf.default
+# echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+# echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+# echo 'src-git diskman https://github.com/jjm2473/luci-app-diskman;dev' >> feeds.conf.default
+# echo 'src-git third_party https://github.com/linkease/istore-packages.git;main' >> feeds.conf.default
+# echo 'src-git jjm2473_apps https://github.com/jjm2473/openwrt-apps.git;main' >> feeds.conf.default
 ## 常用OpenWrt软件包源码合集，同步上游更新！
 ## 通用版luci适合21.02及以上版本
 
