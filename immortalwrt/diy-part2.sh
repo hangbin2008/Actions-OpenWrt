a1ee88e67094edac4496f9940406ac36fc4a806a
hangyubin <hangyubin@163.com>
2024-03-17 20:52:48 +0800

    Create diy-part2.sh

---
+++ immortalwrt/diy-part2.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+
+
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings    # 设置密码为空
+
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
+
+
+# Modify some code adaptation
+#sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' feeds/luci/applications/luci-app-cpufreq/Makefile
+
+# Add autocore support for armvirt
+#sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+
+# Set DISTRIB_REVISION
+#sed -i "s/OpenWrt /hangbin Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 10.10.10.1）
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+
+# Modify system hostname（FROM OpenWrt CHANGE TO OpenWrt-N1）
+# sed -i 's/OpenWrt/OpenWrt-N1/g' package/base-files/files/bin/config_generate
+
+# Replace the default software source
+# sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
+
+sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+
+# 修复部分插件自启动脚本丢失可执行权限问题
+# sed -i '/exit 0/i\chmod +x /etc/init.d/*' package/lean/default-settings/files/zzz-default-settings
+
+# 拉取软件包
+# git clone https://github.com/hangyubin/homeproxy.git package/homeproxy
+# git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# git clone --depth 1 https://github.com/vernesong/OpenClash.git && mv OpenClash/luci-app-openclash ./package && rm -rf OpenClash
+# 删除重复包
+# rm -rf feeds/luci/applications/luci-app-passwall
+# rm -rf feeds/packages/net/haproxy
+# rm -rf feeds/packages/net/mosdns
+# rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/luci/themes/luci-theme-argon
+# rm -rf feeds/luci/applications/luci-app-dockerman
+
+# 其他调整
+# sed -i 's#mount -t cifs#mount.cifs#g' feeds/luci/applications/luci-app-cifs-mount/root/etc/init.d/cifs
+# echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+# echo "CONFIG_PACKAGE_luci=y" >> ./.config
+# echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
+# echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
+# echo "CONFIG_PACKAGE_luci-theme-argon=y" >> ./.config
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
