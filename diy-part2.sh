cc4ca0f4af7c65bb43b150afac1de4da6988b7e9
hangyubin <hangyubin@163.com>
2024-03-17 20:05:01 +0800

    🕊️

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,7 +20,6 @@ else
   sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 fi    
 
-
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
 
